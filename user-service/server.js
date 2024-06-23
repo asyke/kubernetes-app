f050874c83c8178043cba7739c385f36b19d846d
Asylbek Ibrakhimov <112480502+asyke@users.noreply.github.com>
2024-06-23 14:47:15 -0500

    Set up Kubernetes and CI/CD pipeline

---
+++ user-service/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/user");
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use("/api", userRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(5000, () => console.log("User service running on port 5000"))
+  )
+  .catch((err) => console.log(err));
